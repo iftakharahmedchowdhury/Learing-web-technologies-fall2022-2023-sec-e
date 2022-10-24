b623e825a618596438068e1471fb257fbc747136
iftakharahmedchowdhury <iftakharahmedchowdhuryimacc@gmail.com>
2022-10-24 13:25:18 +0600

    Lab 5

---
+++ WT-MID/Lab 5/login.php
@@ -0,0 +1,111 @@
+<html>
+
+<head>
+
+    <title>Document</title>
+</head>
+
+<body>
+    <fieldset>
+        <img width="40px" src="x.png" alt=""> Company
+
+
+
+        <table align="right">
+            <tr>
+                <td>
+                    <a href="">HOME</a>
+                    <a href="">Login</a>
+                    <a href="">Registration</a>
+                </td>
+            </tr>
+        </table>
+
+    </fieldset>
+    <!--     <fieldset>
+        <legend>logIn</legend>
+        <table>
+        
+            <tr>
+                <td>
+                User Name :<input type="text">
+                </td>
+               
+            </tr>
+           
+            <tr>
+        
+              <td>
+              User Password : <input type="password">
+              </td>
+               
+            </tr>
+            <hr>
+            <tr>
+            
+                <td>
+                <input type="checkbox"> Remember me
+                </td>
+            </tr>
+            <tr>
+                
+            </tr>
+        
+
+
+        </table> -->
+    <table align="center">
+
+        <tr>
+            <td>
+                <p> User Name :</p>
+
+            </td>
+
+            <td>
+                <input type="text">
+
+            </td>
+
+        </tr>
+        <br>
+        <tr>
+            <td>
+                <p>User Password :</p>
+                
+            </td>
+            <td>
+                <input type="password">
+             
+               <hr>
+               
+            </td>
+            
+           
+        </tr>
+        
+        <br>
+
+        <tr>
+            <td>
+
+                <input type="checkbox"> Remember me
+
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button type="submit">Submit</button>
+            </td>
+            <td><a href="">Forgot password</a></td>
+        </tr>
+
+    </table>
+    </fieldset>
+
+
+
+</body>
+
+</html>
---
+++ WT-MID/Lab 5/registration.php
@@ -0,0 +1,80 @@
+<html>
+
+<body>
+
+    <fieldset>
+        <img width="40px" src="x.png" alt="">Company
+        <table align="right">
+            <td><a href="Registration.php">Home</a></td>
+            <td><a href="Registration.php">Login</a></td>
+            <td><a href="Registration.php">Registration</a></td>
+        </table>
+        <fieldset>
+            <legend>
+                <h2>Registration</h2>
+            </legend>
+            <table align="center">
+                <tr>
+                    <td>Name :<input type="text" name="name" value="">
+                        <hr width="250px">
+                    </td>
+                </tr>
+
+
+                <tr>
+
+                    <td>Email :<input type="email" name="email" value="">
+                        <hr width="250px">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>User Name :<input type="text" name="id" value="">
+                        <hr width="250px">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Password :<input type="password" name="password" value="">
+                        <hr width="250px">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Confirm Password :<input type="password" name="password" value="">
+                        <hr width="250px">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <fieldset>
+                            <legend>Gender</legend>
+                            <input type="radio" name="gender" value="" /> Male
+                            <input type="radio" name="gender" value="" /> Female
+                            <input type="radio" name="gender" value="" /> Other
+                    </td>
+        </fieldset>
+        </tr>
+
+        <tr>
+            <td>DOB</td>
+            <td><input type="date" name="date" value=""></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td><input type="submit" name="" value="Submit"></td>
+            <td><input type="submit" name="" value="Reset"></td>
+
+        </tr>
+
+        </table>
+    </fieldset>
+    <p>
+        <center>Copyright c 2017</center>
+    </p>
+    </fieldset>
+</body>
+
+</html>
